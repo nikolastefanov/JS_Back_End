9bb295bbe5a39c259001bf5239897ae429ec7695
nikolastefanov <nikolay_stefanov1965@abv.bg>
2023-05-05 04:28:04 +0300

    Meme_LongueTwo

---
+++ Meme_Longue/Meme_Longue2/Meme_Lounge/src/views/profile.js
@@ -0,0 +1,51 @@
+import {html} from'../../node_modules/lit-html/lit-html.js';
+
+import{getMyMemes} from '../api/data.js';
+
+ 
+
+ const memeTemplate=(meme)=html`
+<div class="meme">
+<div class="card">
+    <div class="info">
+        <p class="meme-title">${meme.title}</p>
+        <img class="meme-image" alt="meme-img" src="${meme.imageUrl}">
+    </div>
+    <div id="data-buttons">
+        <a class="button" href="/details/${meme._id}">Details</a>
+    </div>
+</div>
+</div> `;
+
+
+const profileTemplate=(memes,username,email,gender)=>html`
+<section id="user-profile-page" class="user-profile">
+<article class="user-info">
+    <img id="user-avatar-url" alt="user-profile" src="/images/${gender}.png">
+    <div class="user-content">
+        <p>Username: ${username}</p>
+        <p>Email: ${email}</p>
+        <p>My memes count: ${memes.length}</p>
+    </div>
+</article>
+<h1 id="user-listings-title">User Memes</h1>
+<div class="user-meme-listings">
+   
+${memes.length==0?html`<p class="no-memes">No memes in database.</p>`
+                 :memes.map(m=>memeTemplate(m))};
+    
+</div>
+</section> `;
+
+
+
+
+export async function profilePage(ctx){
+
+    const meme=await getMyMemes();
+
+    const username=sessionStorage.getItem('username');
+    const email=sessionStorage.getItem('emial');
+    const gender=sessionStorage.getItem('gender');
+
+}
